bb93b2322148a790373463319e67699555d64c59
Jordan Heskett <jordanheskett@gmail.com>
2020-01-25 09:55:20 -0600

    Seeder Working... refactor later :joy:

--- database/index.js
+++ database/index.js
@@ -1,12 +1,15 @@
 var config;
 try {
   config = require('./config.js')
-} catch {
+}
+catch(error) {
+  console.error(error);
   console.error('config.js Not Found, Reverting to process.env!')
 }
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
+  tinyInt1isBit: false,
   host     : process.env.RDS_HOSTNAME || config.HOSTNAME,
   user     : process.env.RDS_USERNAME || config.USERNAME,
   database : process.env.RDS_DATABASE || config.DATABASE,
@@ -19,8 +22,8 @@ db.connect(function(err) {
     console.error('Database connection failed: ' + err.stack);
     return;
   }
-
   console.log('Connected to database.');
 });
 
 //db.end();
+module.exports = db;
