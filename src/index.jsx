0ac197485d3f2053b2690e33bc76f1a835f8cbc2
Jordan Heskett <jordanheskett@gmail.com>
2020-02-10 10:56:33 -0600

    fixing a few :beetle:

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './components/Reviews.jsx';
 
+
+/* <link href="https://hrr43fecheskett.s3.us-east-2.amazonaws.com/styles/styles.css" rel="stylesheet" /> */
+
+// if (!document.getElementById('reviewsicons')) {
+//   let link = document.createElement('link');
+//   link.id = 'reviewsicons';
+//   link.href = 'https://hrr43fecheskett.s3.us-east-2.amazonaws.com/styles/styles.css';
+//   link.rel = 'stylesheet';
+//   document.head.appendChild(link);
+// }
+
+
 ReactDOM.render(
   <Reviews />, document.getElementById('reviews'),
 );
